1b24bef21c7dcbe5cb0d883bfa5498aa04e4054d
novgunawan <54393790+novgunawan@users.noreply.github.com>
2020-08-05 21:12:05 +0700

    Tambahin files yang lain

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('table.taskTable');
+});
+
+Route::get('/master', function(){
+    return view('adminLTE.master');
+});
+
+Route::get('/data-tables', function () {
+    return view('table.dataTable');
+});
